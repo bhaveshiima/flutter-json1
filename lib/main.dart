15007e10deb2f937c029c2e8e0f11be1b441677e
Bhavesh Patel <bhaveshiima@gmail.com>
2019-02-16 00:45:18 +0530

    Open Detail page with having only title as a user name on that page from listing page using onTap function.

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,6 @@ class MyApp extends StatelessWidget{
 
 class MyHomePage extends StatefulWidget{
   final String title;
-
   MyHomePage({Key key, this.title}) : super (key: key);
 
   @override
@@ -31,24 +30,17 @@ class MyHomePage extends StatefulWidget{
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   Future<List<User>> _getUsers() async{
-
     //http://bhaveshpatel.in/testapi/memberinfo1/2.html
     var JsonURL = 'http://www.json-generator.com/api/json/get/cfwZmvEBbC?indent=2';
     var data = await http.get(JsonURL);
     var jsonData = json.decode(data.body);
 
     List<User> users = [];
-
     for(var u in jsonData){
         User user  = User(u["index"], u["about"], u["name"], u["email"], u["picture"]);
-
         users.add(user);
-
     }
-
-    print(users.length);
     return users;
   }
 
@@ -76,22 +68,37 @@ class _MyHomePageState extends State<MyHomePage> {
               return ListView.builder(
                 itemCount: snapshot.data.length,
                 itemBuilder: (BuildContext context, int index){
+
+                  // Store all values in Variable
+                  var ImgURL = snapshot.data[index].picture;
+                  var UserName = snapshot.data[index].name;
+                  var UserEmail = snapshot.data[index].email;
+
+                  // Return the List View
                   return ListTile(
+                    // Display the Image [ START ]
                     leading: CircleAvatar(
                      backgroundImage: NetworkImage(
-                       snapshot.data[index].picture
+                         ImgURL
                      ),
                     ),
+                    // Display the Image [ END ]
 
-                    title: Text(snapshot.data[index].name),
-                    subtitle: Text(snapshot.data[index].email),
+                    title: Text(UserName),
+                    subtitle: Text(UserEmail),
+                    // On tap or click [ redirect to detail page ] [ START ]
+                    onTap: (){
+                      Navigator.push(context,
+                      new MaterialPageRoute(builder: (context) => DetailPage(snapshot.data[index]))
+                      );
+                    },
+                    // On tap or click [ redirect to detail page ] [ END ]
+
                   );
                 }
               );
 
-            }
-
-
+            } //end of else
           },
         ),
       ),
@@ -99,6 +106,23 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+
+// Detail Page [ START ]
+class DetailPage extends StatelessWidget {
+  final User user;
+
+  DetailPage(this.user);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(user.name),
+        ),
+    );
+  }
+}
+// Detail Page [ END ]
 
 class User{
   final int index;
